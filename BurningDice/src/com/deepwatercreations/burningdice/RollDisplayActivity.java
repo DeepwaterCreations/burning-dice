f7acb770b3f61cb9da4aa69855f26e1f13a0da17
DeepwaterCreations <dylan.craine@deepwatercreations.com>
2013-08-24 15:43:21 -0600

    Fate and Deeds buttons are hooked up.

--- BurningDice/src/com/deepwatercreations/burningdice/RollDisplayActivity.java
+++ BurningDice/src/com/deepwatercreations/burningdice/RollDisplayActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.support.v4.app.NavUtils;
@@ -16,7 +17,7 @@ import android.support.v4.app.NavUtils;
 public class RollDisplayActivity extends Activity {
 
 	private Roll results;
-	
+		
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,12 +25,31 @@ public class RollDisplayActivity extends Activity {
         // Make sure we're running on Honeycomb or higher to use ActionBar APIs
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) 
         	getActionBar().setDisplayHomeAsUpEnabled(true);
-        Resources res = getResources();
-        
-        
+
         Intent intent = getIntent();
         results = (Roll) intent.getSerializableExtra(MainActivity.EXTRA_ROLLRESULTS);
         
+        setupText();
+        
+    }
+    
+    public void spendFate(View view){
+    	results.spendFate();   
+    	setupText();
+    }
+    
+    public void spendDeeds(View view){
+    	results.spendDeeds();
+    	setupText();
+    }
+
+    /**
+     * Sets the text fields to the appropriate numbers.
+     */
+    public void setupText(){
+        Resources res = getResources();
+     
+               
         //String that says "Success!" or "Failure!"
         TextView isSuccessText = (TextView)findViewById(R.id.is_success_field);
         String successorfail = "";    
@@ -56,6 +76,7 @@ public class RollDisplayActivity extends Activity {
         } 
         dieResultsText.setText(dieresults);
         
+        //Difficulty of test earned
         TextView difficultyText = (TextView)findViewById(R.id.difficulty_field);
         String difficulty = "";
         switch(results.getDifficulty()){
@@ -74,38 +95,18 @@ public class RollDisplayActivity extends Activity {
         } 
         difficultyText.setText(difficulty);
     
+        //Number of Successes
         TextView numSuccessText = (TextView)findViewById(R.id.num_success_field);
         String numsuccess = String.valueOf(results.getMargin());
         numSuccessText.setText(numsuccess);
         
+        //Obstacle
         TextView obText = (TextView)findViewById(R.id.ob_field);
         String ob = String.valueOf(results.getObstacle());
         obText.setText(ob);
-        
-//        LinearLayout llayout = new LinearLayout(this);
-//        
-//        TextView successesview = new TextView(this);
-//        successesview.setTextSize(30);
-//        CharSequence successestext = "You got " + results.getNumSuccesses() + " successes on " + results.getTotalDice() + " dice!";
-//        successesview.setText(successestext);
-//        
-//        TextView resultsview = new TextView(this);
-//        successesview.setTextSize(30);
-//        String resultstextS = "Results: ";
-//        for(int num : results.getResults()){
-//        	resultstextS += num;
-//        	resultstextS += " ";
-//        }
-//        CharSequence resultstext = resultstextS;
-//        resultsview.setText(resultstext);
-//        
-//        llayout.addView(successesview);
-//        llayout.addView(resultsview);
-//                
-//        setContentView(llayout);        
-        
     }
-
+    
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_roll_display, menu);
